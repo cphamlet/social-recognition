284682529fb1aeddb531fb53533594bf7a0c1b58
Chad Thomas <chad.thomas.ii@gmail.com>
2017-11-05 01:49:13 -0500

    Added functionality to upload image to server

---
+++ Front End/assets/js/faceStats.js
@@ -0,0 +1,44 @@
+$(document).on('change', '#file', function()
+{
+    var property = document.getElementById("file").files[0]; //Get the file
+    var image_name = property.name; //Get the name of the file
+    var image_extension = image_name.split(".").pop().toLowerCase();
+    if(jQuery.inArray(image_extension, ['gif', 'png', 'jpg', 'jpeg']) == -1) //Check to make sure it's the correct type
+    {
+        alert("Invalid Image File");
+    }
+
+    //Check for image size not over 2 megs
+    var image_size = property.size;
+    if(image > 2000000)
+    {
+        alert("Image File Size is to big");
+    }
+    else
+    {
+        //Make a formdata object and add the file to it
+        var form_data = new FormData();
+        form_data.append("file", property);
+
+        $.ajax
+        ({
+            url: "/upload.php",
+            method: "POST",
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            success:function(data)
+            {
+                var info = JSON.parse(data);
+                console.log(info);
+
+                //$('.image-placeholder').css("background-image", "url(" + info.name + ")");
+            }
+
+            
+        })
+
+    }
+
+});
