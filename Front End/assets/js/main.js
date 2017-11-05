7fda392b49b5ef3c22f6eeaefc04fc219d07069a
romano-w <romanow@berea.edu>
2017-11-04 21:38:11 -0400

    Updated JavaScript functions to POST properly

--- Front End/assets/js/main.js
+++ Front End/assets/js/main.js
@@ -1,20 +1,31 @@
 
-function upload() {
-	alert("THIS RAN");
-	var formData = new FormData();
-	formData.append("file", document.getElementById("file").files[0]);
-	console.log(document.getElementById("file").files[0]);
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "http://ec2-34-210-241-233.us-west-2.compute.amazonaws.com/upload");
-	xhr.send(formData);
+var formData = new FormData();
+formData.append("file", document.getElementById("file").files[0]);
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+        alert(xhr.responseText);
+    }
 }
+xhr.open("POST", "http://ec2-34-210-241-233.us-west-2.compute.amazonaws.com/upload");
+xhr.send(formData);
 
-$.ajax({
-  type: "POST",
-  url: "http://ec2-34-210-241-233.us-west-2.compute.amazonaws.com/upload",
-  data: document.getElementById("file").files[0],
-  cache: false,
-  success: function(data){
-     $("#resultarea").text(data);
-  }
-});
+// function upload() {
+// 	alert("THIS RAN");
+// 	var formData = new FormData();
+// 	formData.append("file", document.getElementById("file").files[0]);
+// 	console.log(document.getElementById("file").files[0]);
+// 	var xhr = new XMLHttpRequest();
+// 	xhr.open("POST", "http://ec2-34-210-241-233.us-west-2.compute.amazonaws.com/upload");
+// 	xhr.send(formData);
+// }
+
+// $.ajax({
+//   type: "POST",
+//   url: "http://ec2-34-210-241-233.us-west-2.compute.amazonaws.com/upload",
+//   data: document.getElementById("file").files[0],
+//   cache: false,
+//   success: function(data){
+//      $("#resultarea").text(data);
+//   }
+// });
