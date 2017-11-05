b8960ac70aae0c662e0585781fd2d2fe3939336a
Chad Thomas <chad.thomas.ii@gmail.com>
2017-11-05 03:31:39 -0500

    Fixed parse error

--- Front End/upload.php
+++ Front End/upload.php
@@ -2,9 +2,9 @@
 
 if($_FILES['file']['name'] != '')
 {
-    $test = explode(".", $_FILES["file"]["name"]); // 
+    $test = explode(".", $_FILES["file"]["name"]); 
     $extension = end($test);
-    $name = rand(100,999); . '.' . $extension;
+    $name = rand(100,999) . '.' . $extension;
     $location = '/img' .$name;
     move_uploaded_file($_FILES["file"]["tmp_name"], $location);
 
