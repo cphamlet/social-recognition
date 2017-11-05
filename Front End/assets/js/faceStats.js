7503b811f4035034e9240cf23924c9043e15ca37
Chad Thomas <chad.thomas.ii@gmail.com>
2017-11-05 03:21:54 -0500

    Redeploy with php active

--- Front End/assets/js/faceStats.js
+++ Front End/assets/js/faceStats.js
@@ -1,8 +1,28 @@
 $(document).on('change', '#file', function()
 {
     var property = document.getElementById("file").files[0]; //Get the file
-   
-    
+    var image_name = property.name; //Get the name of the file
+    var image_extension = image_name.split(".").pop().toLowerCase();
+    if(jQuery.inArray(image_extension, ['gif', 'png', 'jpg', 'jpeg']) == -1) //Check to make sure it's the correct type
+    {
+        alert("Invalid Image File");
+    }
+
+    //Check for image size not over 2 megs
+    var image_size = property.size;
+    console.log(image_size);
+    if(image_size > 20000000) 
+    {
+        alert("Image File Size is to big");
+    }
+    else
+    {
+        console.log(property);
+        //Make a formdata object and add the file to it
+        var form_data = new FormData();
+        form_data.append("file", property);
+        console.log(form_data);
+
         $.ajax
         ({
             url: "././upload.php",
@@ -21,6 +41,6 @@ $(document).on('change', '#file', function()
             
         })
 
-    
+    }
 
 });
--- Front End/upload.php
+++ Front End/upload.php
@@ -1,10 +1,18 @@
 <?php
 
+if($_FILES['file']['name'] != '')
+{
+    $test = explode(".", $_FILES["file"]["name"]); // 
+    $extension = end($test);
+    $name = rand(100,999); . '.' . $extension;
+    $location = '/img' .$name;
+    move_uploaded_file($_FILES["file"]["tmp_name"], $location);
 
-// $data['file'] = $_FILES;
+    var $arr = [];
 
-// echo json_encode($data);
+    $arr['location'] = $location;
+    $arr['name'] = $name;
 
+    echo json_encode($data);
     
-  
-    echo "It works ";
+}
