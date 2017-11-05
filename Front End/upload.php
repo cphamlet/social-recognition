7248dcd338dee6bc6c734a615e319b412052e89d
Chad Thomas <chad.thomas.ii@gmail.com>
2017-11-05 03:46:08 -0500

    Fixed slashes

--- Front End/upload.php
+++ Front End/upload.php
@@ -5,7 +5,7 @@ if($_FILES['file']['name'] != '')
     $test = explode(".", $_FILES["file"]["name"]); 
     $extension = end($test);
     $name = rand(100,999) . '.' . $extension;
-    $location = '/img/' . $name;
+    $location = "/".'img'.'/' . $name;
     move_uploaded_file($_FILES["file"]["tmp_name"], $location);
 
     $arr = [];
