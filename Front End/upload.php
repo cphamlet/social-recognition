868c899a13e269384da5fd2ecba434ec838b1355
Chad Thomas <chad.thomas.ii@gmail.com>
2017-11-05 04:02:57 -0500

    Test

--- Front End/upload.php
+++ Front End/upload.php
@@ -5,15 +5,15 @@ if($_FILES['file']['name'] != '')
     $test = explode(".", $_FILES["file"]["name"]); 
     $extension = end($test);
     $name = rand(100,999) . '.' . $extension;
-    $root = '/';
-    $location = $root .'img'. $root . $name;
-    move_uploaded_file($_FILES["file"]["tmp_name"], $location);
+    $location = "/".'img'.'/' . $name;
+    $result = stripslashes($location);
+    move_uploaded_file($_FILES["file"]["tmp_name"], $result);
 
     $arr = [];
 
     $arr['location'] = $location;
     $arr['name'] = $name;
 
-    echo json_encode($arr, JSON_UNESCAPED_SLASHES);
+    echo json_encode($arr);
     
 }
