5b529edd42a3c5cf5dff2202e913aa17d04996d3
Chad Thomas <chad.thomas.ii@gmail.com>
2017-11-05 08:14:13 -0500

    Added colons

--- Front End/assets/js/faceStats.js
+++ Front End/assets/js/faceStats.js
@@ -94,9 +94,9 @@ $(document).on('change', '#file', function()
                         }
 
                       
-                        $("#sex").text("Gender " + jsUcfirst(data[0]['faceAttributes'].gender));
-                        $("#age").text("Age " + data[0]['faceAttributes'].age);
-                        $("#glasses").text("Glasses? " + data[0]['faceAttributes'].glasses);
+                        $("#sex").text("Gender: " + jsUcfirst(data[0]['faceAttributes'].gender));
+                        $("#age").text("Age: " + data[0]['faceAttributes'].age);
+                        $("#glasses").text("Glasses: " + data[0]['faceAttributes'].glasses);
                         
                         
 
