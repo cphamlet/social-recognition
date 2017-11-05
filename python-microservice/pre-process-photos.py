7f3b704148b91d7cf09b3cfa49532833fa965c07
John Doe <johndoe@example.com>
2017-11-05 05:51:38 -0500

    added photo resizing

--- python-microservice/pre-process-photos.py
+++ python-microservice/pre-process-photos.py
@@ -1,16 +1,22 @@
 import face_recognition, json
 from os import listdir
 from os.path import isfile, join, dirname, abspath
+from PIL import Image, ImageOps
 
-
+target_dir = "/known-pics"
 processed_data = {"files": []}
 
-mypath = dirname(abspath(__file__))+"/known-pics"
+mypath = dirname(abspath(__file__))+target_dir
 fileList = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 for photo in fileList:
+    maxsize = (640,640)
+    image = Image.open(mypath+"/"+photo)
+    thumb = ImageOps.fit(image, maxsize, Image.ANTIALIAS)
+    thumb.save(mypath+"/"+photo)
+
     print("Classifying: "+ photo)
-    known_image = face_recognition.load_image_file("./known-pics/"+photo, mode='RGB')
+    known_image = face_recognition.load_image_file("."+target_dir+"/"+photo, mode='RGB')
     temp_code = face_recognition.face_encodings(known_image)
     if len(temp_code) == 0:
         print("\n"+photo+" is not being parsed right for some reason")
