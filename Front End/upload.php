f0ff21a299f713a317ea2c2ec3f0b0d03fde14cb
Chad Thomas <chad.thomas.ii@gmail.com>
2017-11-05 03:54:34 -0500

    Changed root

--- Front End/upload.php
+++ Front End/upload.php
@@ -5,9 +5,9 @@ if($_FILES['file']['name'] != '')
     $test = explode(".", $_FILES["file"]["name"]); 
     $extension = end($test);
     $name = rand(100,999) . '.' . $extension;
-    $location = "/".'img'.'/' . $name;
-    $result = stripslashes($location);
-    move_uploaded_file($_FILES["file"]["tmp_name"], $result);
+    $root = '/';
+    $location = $root .'img'. $root . $name;
+    move_uploaded_file($_FILES["file"]["tmp_name"], $location);
 
     $arr = [];
 
